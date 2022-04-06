05d2db25d9a5fbee81464e8a798db7f7d283c26c
jesslynowen7 <owenjesslyn@gmail.com>
2022-04-06 08:13:50 +0700

    create function insert hotel

--- model/hotel.go
+++ model/hotel.go
@@ -4,9 +4,9 @@ type Hotel struct {
 	HotelID      int    `json:"hotel_id,omitempty"`
 	HotelName    string `json:"hotel_name,omitempty"`
 	HotelCity    string `json:"hotel_city,omitempty"`
-	HotelAddress string `json:"hotel_Addres,omitempty"`
+	HotelAddress string `json:"hotel_Address,omitempty"`
 	HotelPhone   string `json:"hotel_phone,omitempty"`
-	Rooms        []Room `json:"rooms,omitempty"`
+	// Rooms        []Room `json:"rooms,omitempty"`
 }
 type HotelResponse struct {
 	Status  int    `json:"status"`
