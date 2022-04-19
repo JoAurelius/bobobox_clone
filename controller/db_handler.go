df45c6fdbde994b55e054cb1c33c73dbd794df10
JoAurelius <jonathan.arls@yahoo.com>
2022-04-19 08:31:02 +0700

    add env

--- controller/db_handler.go
+++ controller/db_handler.go
@@ -2,13 +2,20 @@ package controller
 
 import (
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func connect() *gorm.DB {
-	dsn := "root:@tcp(localhost:3306)/db_bobobox?parseTime=true&loc=Asia%2fJakarta"
+	errEnv := godotenv.Load()
+	if errEnv != nil {
+		log.Fatal("Error loading .env file for database	connection")
+		return nil
+	}
+	dsn := os.Getenv("DB")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-co-op/gocron v1.13.0
 	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.4.0
 	github.com/rs/cors v1.8.2
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	gorm.io/driver/mysql v1.3.2
